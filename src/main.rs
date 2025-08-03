af70d57db9cdcbc45f6720b9ae3a3fd928f88bce
Taice <tai.stanic@gmail.com>
2025-08-03 18:35:54 +0200

    udate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod settings;
+use std::{collections::LinkedList, f32};
+
 use settings::Settings;
 
 use macroquad::prelude::*;
@@ -18,110 +20,161 @@ fn window_conf() -> Conf {
 async fn main() {
     let mut sudoku = Sudoku::default();
     let mut settings = Settings::default();
+    let font = load_ttf_font_from_bytes(include_bytes!("../assets/Roboto-Regular.ttf")).unwrap();
     loop {
         clear_background(GRAY);
-        sudoku.draw(&settings);
-        next_frame().await
+        sudoku.draw(&settings, &font);
+        next_frame().await;
     }
 }
 
-#[derive(Default)]
 pub struct Sudoku {
-    nums: [[u8; 9]; 9],
+    nums: [[u16; 9]; 9],
+}
+
+impl Default for Sudoku {
+    fn default() -> Self {
+        Self {
+            nums: [[0b111111111; 9]; 9],
+        }
+    }
 }
 
 impl Sudoku {
-    pub fn draw(&self, s: &Settings) {
-        let min_size = f32::min(screen_width(), screen_height());
-        let box_size = s.get_box_size(min_size);
+    pub fn draw(&self, s: &Settings, font: &Font) {
+        let min_len = f32::min(screen_width(), screen_height());
+        let (side, box_size) = s.get_lengths(min_len);
 
-        draw_rectangle(0., 0., min_size, min_size, WHITE);
+        draw_rectangle(0., 0., side, side, WHITE);
+        draw_inlines(s, side, box_size);
+        draw_box_lines(s, side, box_size);
+        draw_outlines(s, side);
 
-        let mut half = s.outer_line / 2.;
-        // Draw Sudoku lines
-        draw_line(0.0, half, min_size, half, s.outer_line, s.line_color); // TOP_LEFT_RIGHT
-        draw_line(half, 0.0, half, min_size, s.outer_line, s.line_color); // TOP_LEFT_BOT
-        draw_line(
-            min_size,
-            0.0,
-            min_size,
-            min_size,
-            s.outer_line,
-            s.line_color,
-        ); // TOP_RIGHT_BOT    
-        draw_line(
-            half,
-            min_size - half,
-            min_size,
-            min_size - half,
-            s.outer_line,
-            s.line_color,
-        ); // BOT_LEFT_RIGHT
+        self.draw_grid(s, box_size, font);
+    }
 
-        for n in 1..=8 {
-            let point;
-            let width;
-            let i = n as f32;
-            if n % 3 == 0 {
-                half = s.box_line / 2.;
-                point = s.outer_line
-                    + half
-                    + i * box_size
-                    + i / 3. * s.box_line
-                    + (i - i / 3.) * s.normal_line;
-                width = s.box_line;
-            } else {
-                half = s.normal_line / 2.;
-                point = s.outer_line
-                    + half
-                    + i * box_size
-                    + (n / 3) as f32 * s.box_line
-                    + (n - (n / 3)) as f32 * s.normal_line;
-                width = s.normal_line;
-            }
-            draw_line(0.0, point, min_size, point, width, s.line_color);
-            draw_line(point, 0.0, point, min_size, width, s.line_color);
-        }
-
-        let mut x = s.outer_line;
+    fn draw_grid(&self, s: &Settings, box_size: f32, font: &Font) {
+        let text_params = TextParams {
+            font: Some(font),
+            font_size: s.get_num_font_size(box_size),
+            font_scale: s.num_font_scale,
+            color: s.font_color,
+            ..Default::default()
+        };
         let mut y = s.outer_line;
+        let mut x = s.outer_line;
 
         for (i, row) in self.nums.iter().enumerate() {
-            y = s.outer_line
-                + (i / 3) as f32 * s.box_line
-                + (i - (i / 3)) as f32 * s.normal_line
-                + i as f32 * box_size;
-            if i != 0 {
-                if i % 3 == 0 {
-                    y += s.box_line;
-                } else {
-                    y += s.normal_line;
-                }
-            }
+            let num_y = y + s.get_y_num_offset(box_size);
+            let note_y = y + s.get_y_note_offset(box_size);
             for (j, n) in row.iter().enumerate() {
-                draw_text_ex(
-                    &self.nums[i][j].to_string(),
-                    x,
-                    y,
-                    TextParams {
-                        font_size: s.font_size,
-                        font_scale: s.get_font_scale(box_size),
-                        color: s.font_color,
-                        ..Default::default()
-                    },
-                );
-                draw_rectangle(x, y, 4., 4., GREEN);
-                x += box_size;
-                if j % 3 == 0 {
+                // note
+                if n & (1 << 15) == 0 {
+                    let x = x + s.get_x_note_offset(box_size);
+                    draw_notes(s, box_size, x, note_y, *n, font);
+                // num
+                } else {
+                    let x = x + s.get_x_num_offset(box_size);
+                    draw_text_ex(&n.to_string(), x, num_y, text_params.clone());
+                }
+
+                if j % 3 == 2 {
                     x += s.box_line;
                 } else {
                     x += s.normal_line;
                 }
+                x += box_size;
             }
             x = s.outer_line;
 
+            if i % 3 == 2 {
+                y += s.box_line;
+            } else {
+                y += s.normal_line;
+            }
             y += box_size;
         }
-        println!();
+    }
+}
+
+pub fn draw_box_lines(s: &Settings, side: f32, box_size: f32) {
+    let mut point = box_size + s.outer_line;
+    for n in 1..9 {
+        if n % 3 == 0 {
+            draw_rectangle(point, 0.0, s.box_line, side, s.box_color);
+            draw_rectangle(0.0, point, side, s.box_line, s.box_color);
+            point += s.box_line;
+        } else {
+            point += s.normal_line;
+        }
+
+        point += box_size;
+    }
+}
+
+pub fn draw_inlines(s: &Settings, side: f32, box_size: f32) {
+    let mut point = box_size + s.outer_line;
+    for n in 1..9 {
+        if n % 3 != 0 {
+            draw_rectangle(point, 0.0, s.normal_line, side, s.normal_color);
+            draw_rectangle(0.0, point, side, s.normal_line, s.normal_color);
+            point += s.normal_line;
+        } else {
+            point += s.box_line;
+        }
+
+        point += box_size;
+    }
+}
+
+pub fn draw_outlines(s: &Settings, side: f32) {
+    let half = s.outer_line / 2.;
+    //Draw Sudoku lines
+    draw_line(0.0, half, side, half, s.outer_line, s.outer_color); // TOP_LEFT_RIGHT
+    draw_line(half, 0.0, half, side, s.outer_line, s.outer_color); // TOP_LEFT_BOT
+    draw_line(
+        side - half,
+        0.0,
+        side - half,
+        side,
+        s.outer_line,
+        s.outer_color,
+    ); // TOP_RIGHT_BOT    
+    draw_line(
+        half,
+        side - half,
+        side,
+        side - half,
+        s.outer_line,
+        s.outer_color,
+    ); // BOT_LEFT_RIGHT
+}
+
+pub fn draw_notes(s: &Settings, box_size: f32, x: f32, y: f32, num: u16, font: &Font) {
+    let text_params = TextParams {
+        font: Some(font),
+        font_size: s.get_note_font_size(box_size),
+        font_scale: s.note_font_scale,
+        color: s.note_color,
+        ..Default::default()
+    };
+
+    let note_size = box_size / 3.;
+    let mut coords = (x, y);
+    for i in 0..3 {
+        for j in 0..3 {
+            let n = i * 3 + j;
+            if num & (1 << n) > 0 {
+                draw_text_ex(
+                    &(n + 1).to_string(),
+                    coords.0,
+                    coords.1,
+                    text_params.clone(),
+                );
+            }
+            coords.0 += note_size;
+        }
+        coords.0 = x;
+        coords.1 += note_size;
     }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,35 +1,77 @@
 use macroquad::prelude::*;
 
-const BASE_BOX_SIZE: f32 = (500 - 8 - 8 - 12) as f32 / 9.;
+pub const BASE_BOX_SIZE: f32 = (500 - 8 - 8 - 12) as f32 / 9.;
 
 pub struct Settings {
     pub outer_line: f32,
     pub box_line: f32,
     pub normal_line: f32,
-    pub line_color: Color,
+
+    pub outer_color: Color,
+    pub box_color: Color,
+    pub normal_color: Color,
+
+    pub num_font_scale: f32,
+    pub note_font_scale: f32,
+    num_font_size: u16,
+    note_font_size: u16,
     pub font_color: Color,
-    pub font_size: u16,
+    pub note_color: Color,
 }
 
 impl Default for Settings {
     fn default() -> Self {
         Self {
-            outer_line: 4.0,
+            outer_line: 5.0,
             box_line: 4.0,
             normal_line: 2.0,
-            line_color: BLACK,
-            font_color: DARKGRAY,
-            font_size: 20,
+            outer_color: BLACK,
+            box_color: BLACK,
+            normal_color: DARKGRAY,
+            font_color: Color {
+                r: 0.2,
+                g: 0.2,
+                b: 0.2,
+                a: 1.0,
+            },
+
+            num_font_size: 120,
+            num_font_scale: 0.5,
+
+            note_font_size: 33,
+            note_font_scale: 0.5,
+
+            note_color: DARKGRAY,
         }
     }
 }
 
 impl Settings {
-    pub fn get_box_size(&self, min_size: f32) -> f32 {
-        (min_size - self.outer_line * 2. - self.box_line * 2. - self.normal_line * 6.) / 9.
+    pub fn get_lengths(&self, min_size: f32) -> (f32, f32) {
+        let offset = self.outer_line * 2.0 + self.box_line * 2.0 + self.normal_line * 6.0;
+        let s = min_size - offset;
+        let rem = s % 9.;
+        let a = min_size - rem;
+        (a, ( s - rem ) / 9.)
     }
 
-    pub fn get_font_scale(&self, box_size: f32) -> f32 {
-        self.font_size as f32 / (box_size / BASE_BOX_SIZE)
+    pub fn get_num_font_size(&self, box_size: f32) -> u16 {
+        (self.num_font_size as f32 * (box_size / BASE_BOX_SIZE)) as u16
+    }
+    pub fn get_note_font_size(&self, box_size: f32) -> u16 {
+        (self.note_font_size as f32 * (box_size / BASE_BOX_SIZE)) as u16
+    }
+
+    pub fn get_x_num_offset(&self, box_size: f32) -> f32 {
+        10.0 * (box_size / BASE_BOX_SIZE)
+    }
+    pub fn get_x_note_offset(&self, box_size: f32) -> f32 {
+        4.0 * (box_size / BASE_BOX_SIZE)
+    }
+    pub fn get_y_num_offset(&self, box_size: f32) -> f32 {
+        (BASE_BOX_SIZE - 5.) * (box_size / BASE_BOX_SIZE)
+    }
+    pub fn get_y_note_offset(&self, box_size: f32) -> f32 {
+        (BASE_BOX_SIZE - 38.) * (box_size / BASE_BOX_SIZE)
     }
 }
